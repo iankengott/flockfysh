b601504f23c8a77ba4a484fbee8750c25c75df25
raymondytian <rymdtian@gmail.com>
2022-07-26 15:16:32 -0700

    begin yahoo scraper

---
+++ scraper/yahoo_images/crawler.py
@@ -0,0 +1,87 @@
+from urllib.parse import quote
+import shutil
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+import time
+import json
+
+BASE_URL = "https://www.bing.com/images/search?"
+
+
+def gen_query_url(keywords, filters, extra_query_params=''):
+    keywords_str = "&q=" + quote(keywords)
+    query_url = BASE_URL + keywords_str
+    if len(filters) > 0:
+        query_url += "&qft="+filters
+    query_url += extra_query_params
+    return query_url
+
+
+def image_url_from_webpage(driver, max_number=10000):
+    image_urls = list()
+
+    time.sleep(1)
+    img_count = 0
+
+    while True:
+        image_elements = driver.find_elements_by_class_name("iusc")
+        if len(image_elements) > max_number:
+            break
+        if len(image_elements) > img_count:
+            img_count = len(image_elements)
+            driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);")
+        else:
+            smb = driver.find_elements_by_class_name("btn_seemore")
+            if len(smb) > 0 and smb[0].is_displayed():
+                smb[0].click()
+            else:
+                break
+        time.sleep(1)
+    for image_element in image_elements:
+        m_json_str = image_element.get_attribute("m")
+        m_json = json.loads(m_json_str)
+        image_urls.append(m_json["murl"])
+    return image_urls
+
+
+def crawl_image_urls(keywords, filters, max_number=10000, proxy=None, proxy_type="http", extra_query_params=''):
+    chrome_path = shutil.which("chromedriver")
+    chrome_path = "./bin/chromedriver" if chrome_path is None else chrome_path
+    
+    #Modified from original to make headless
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.headless = False 
+
+    if proxy is not None and proxy_type is not None:
+        chrome_options.add_argument(
+            "--proxy-server={}://{}".format(proxy_type, proxy))
+    
+    #Update to handle webdriver
+    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), chrome_options=chrome_options)
+
+    query_url = gen_query_url(keywords, filters, extra_query_params=extra_query_params)
+    driver.set_window_size(3840, 2160)
+    driver.get(query_url)
+    image_urls = image_url_from_webpage(driver, max_number)
+    driver.close()
+
+    len_urls_unfilter = len(image_urls)
+
+    if max_number > len(image_urls):
+        output_num = len(image_urls)
+    else:
+        output_num = max_number
+
+    print("Crawled {} image urls.".format(
+        len(image_urls)))
+
+    return image_urls[0:output_num], len_urls_unfilter
+
+
+if __name__ == '__main__':
+    images = crawl_image_urls(
+        "cat png", "+filterui:aspect-square", max_number=10)
+    for i in images:
+        print(i+"\n")
---
+++ scraper/yahoo_images/util.py
@@ -0,0 +1,105 @@
+import requests
+import shutil
+import posixpath
+import urllib
+import os
+import imghdr
+import validators
+import base64
+import uuid
+
+from PIL import Image
+
+DEFAULT_OUTPUT_DIR = "yahoo-images"
+
+
+def valid_image(file_path: str) -> None:
+    file_path = file_path
+    raw_name, ext = os.path.splitext(file_path)
+    img_type = imghdr.what(file_path) or "jpeg"
+    if f".{img_type}" != ext:
+        new_file_path = f"{raw_name}.{img_type}"
+        shutil.move(file_path, new_file_path)
+        file_path = new_file_path
+    try:
+        img = Image.open(file_path).convert("RGB")
+        img.save(file_path)
+    except:
+        os.remove(file_path)
+
+
+def get_uuid() -> str:
+    return str(uuid.uuid4().hex)
+
+
+def download_image(url, path) -> bool:
+    try:
+        r = requests.get(url, stream=True, timeout=10, verify=False)
+        if r.ok:
+            ext = r.headers['Content-Type'].split("/")[-1].strip()
+            filename = os.path.join(path, f'{get_uuid()}.{ext}')
+            print(filename)
+            with open(filename, 'wb') as f:
+                r.raw.decode_content = True
+                shutil.copyfileobj(r.raw, f)
+                # f.write(r.content)
+            valid_image(filename)
+        else:
+            return False
+    except Exception as e:
+        return False 
+
+
+def get_file_name(url, index, prefix='image') -> str:
+    try:
+        path = urllib.parse.urlsplit(url).path
+        filename = posixpath.basename(path).split('?')[0]
+        type, _ = file_data(filename)
+        result = "{}_{}.{}".format(prefix, index, type)
+        return result
+    except Exception as e:
+        print("[!] Get file name: {}\n[!] Err :: {}".format(url, e))
+        return prefix
+
+
+def rename(name, index, prefix='image') -> str:
+    try:
+        type, _ = file_data(name)
+        result = "{}_{}.{}".format(prefix, index, type)
+        return result
+    except Exception as e:
+        print("[!] Rename: {}\n[!] Err :: {}".format(name, e))
+        return prefix
+
+
+def file_data(name):
+    try:
+        type = name.split(".")[-1]
+        name = name.split(".")[0]
+        if type.lower() not in ["jpe", "jpeg", "jfif", "exif", "tiff", "gif", "bmp", "png", "webp", "jpg"]:
+            type = "jpg"
+        return (type, name)
+    except Exception as e:
+        print("[!] Issue getting: {}\n[!] Err :: {}".format(name, e))
+        return (name, "jpg")
+
+
+def make_image_dir(output_dir, force_replace=False) -> str:
+    image_dir = output_dir
+    if len(output_dir) < 1:
+        image_dir = os.path.join(os.getcwd(), DEFAULT_OUTPUT_DIR)
+
+    if force_replace:
+        if os.path.isdir(image_dir):
+            shutil.rmtree(image_dir)
+    try:
+        if not os.path.isdir(image_dir):
+            os.makedirs(image_dir)
+    except:
+        pass
+
+    return image_dir
+
+
+if __name__ == '__main__':
+    print("util")
