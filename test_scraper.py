8836e7e7c7ed35a8a958a5cbe94c05b98af2261d
raymondytian <rymdtian@gmail.com>
2022-07-27 15:24:38 -0700

    update test scraper

--- test_scraper.py
+++ test_scraper.py
@@ -5,7 +5,6 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), 'scraper'))
 
 from run import parse_params, get_input_file
-from scraper.bing_images import bing
 from scraper.WebDataLoader import WebDataLoader
 
 input_config_yaml = parse_params(get_input_file())
